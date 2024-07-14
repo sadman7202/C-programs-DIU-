6a21c901d8788e9581bcbcb062db677de8157c01
sadman7202 <royalsakib2@gmail.com>
2024-07-14 23:01:25 +0600

    new

---
+++ C++/pattern/diamond.cpp
@@ -0,0 +1,39 @@
+#include<iostream>>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter how many row : ";
+    cin>>n;
+    for(int row=1;row<=n;row++){
+        for(int col=1;col<=n-row;col++){
+            cout<<" ";
+        }
+        for(int col=1;col<=2*row-1;col++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+     for (int row = n - 1; row >= 1; row--) {
+        for (int col = 1; col <= n - row; col++) {
+            cout << " ";
+        }
+        for (int col = 1; col <= 2 * row - 1; col++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ C++/pattern/hollo_rectancge.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter size of the rectangle: ";
+    cin>>n;
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=n;j++){
+                if(i==0||i==n||j==0||j==n)
+                    cout<<"*";
+                else
+                    cout<<" ";
+
+        }
+        cout<<endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
